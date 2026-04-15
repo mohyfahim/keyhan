a32de2d66cd6efe59e5fab3b20217f36301a6971
mohyfahim <fahimohy@gmail.com>
2026-04-15 23:21:09 +0330

    feat: project structure

---
+++ example/esp32/main/main.c
@@ -0,0 +1,7 @@
+#include "ota.h"
+#include <stdio.h>
+
+void app_main(void) {
+  printf("hello world\r\n");
+  say_hello();
+}
---
+++ src/ota.c
@@ -0,0 +1,3 @@
+#include "ota.h"
+
+void say_hello() { printf("hello from ota\r\n"); }
